a59eb44d17c6ab6a4d4d66df0c107f0a29662d92
SinisterX5 <rohan1.785401@ac.tu.edu.np>
2021-08-31 15:41:07 +0545

    test

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello world!");
+    printf("Hello worlds!");
     printf("This is an edit!");
 printf("This is sandeshsss");
     printf("This is Dilasha test");
