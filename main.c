e7977905b647f25e7cf3832ae7b637f7ecf990bd
SinisterX5 <rohan1.785401@ac.tu.edu.np>
2021-08-31 16:38:24 +0545

    Reset everything to default

--- main.c
+++ main.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-
-int main(){
-    printf("Hello worlds!");
-    printf("This is an edit!");
-printf("This is sandeshsss");
-    printf("This is Dilasha test");
-test
-    printf("This is me Suyog");
-
-
-}
+#include<stdio.h>
